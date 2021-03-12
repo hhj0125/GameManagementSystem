65196896141a45295361e2864c8116cfdb47d7da
hhj0125 <gjgywns0125@naver.com>
2021-03-12 22:44:10 +0900

    123

---
+++ src/GameManagementSystem/MenuManager.java
@@ -0,0 +1,13 @@
+package GameManagementSystem;
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+
+		System.out.println("1. Add User");
+		System.out.println("2. Delete User");
+		System.out.println("3. Edit User");
+		System.out.println("4. View User");
+		System.out.println("5. Exit");
+	}
+}
